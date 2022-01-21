9370d7a28ecea575ba8c3eb96932f3615a6e03e6
llwafelll <motorolamarcin@gmail.com>
2022-01-21 19:45:10 +0100

    serializer.py definition.

--- FlashcardsApp/flashcards/api/serializers.py
+++ FlashcardsApp/flashcards/api/serializers.py
@@ -3,16 +3,28 @@ from models import User, Deck, Card, Session
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'surname', 'age', 'date_created',
+                  'date_visited', 'date_updated', 'color', 'starred')
 
 
 class DeckSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Deck
+        fields = ('id', 'name', 'number_cards', 'date_created', 'last_visited',
+                  'last_updated', 'color', 'starred')
 
 
-class DeckSerializer(serializers.ModelSerializer):
-    pass
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ('id', 'front', 'back', 'color', 'starred', 'suspended', 
+                  'date_created', 'date_visited', 'last_updated', 'state')
 
 
 class SessionSerilizer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Session
+        fields = ('id', 'time_spend', 'number_correct_answers',
+                  'number_studied_cards')
